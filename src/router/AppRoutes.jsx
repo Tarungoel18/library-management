4cc7834f24a52c792ec5f10d88a1746cad8bc4e0
Tarun Goel <tarungoel1823@gmail.com>
2026-01-30 17:46:39 +0530

    lazy loading

--- src/router/AppRoutes.jsx
+++ src/router/AppRoutes.jsx
@@ -1,23 +1,31 @@
-import { Routes, Route , Navigate} from "react-router-dom";
-import BookList from "../pages/book-list/Index.jsx";
-import StudentList from "../pages/student-list/Index.jsx";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { lazy, Suspense } from "react";
 import AppLayout from "../layout/AppLayout.jsx";
-import StudentDetail from "../pages/student-detail/Index.jsx";
-import NotFound from "../pages/not-found/Index.jsx";
 import { ROUTES } from "../constants/routes.js";
+
+const BookList = lazy(() => import("../pages/book-list/Index.jsx"));
+const StudentList = lazy(() => import("../pages/student-list/Index.jsx"));
+const StudentDetail = lazy(() => import("../pages/student-detail/Index.jsx"));
+const NotFound = lazy(() => import("../pages/not-found/Index.jsx"));
+
 const AppRoutes = () => {
   return (
-    <Routes>
-      <Route element={<AppLayout />}> 
-        <Route path={ROUTES.BOOK} element={<BookList />} />
-        <Route path={ROUTES.STUDENT}>
-          <Route index element={<StudentList />} />
-          <Route path=":id" element={<StudentDetail />} />
+    <Suspense fallback={<div>Loading..</div>}>
+      <Routes>
+        <Route element={<AppLayout />}>
+          <Route path={ROUTES.BOOK} element={<BookList />} />
+          <Route path={ROUTES.STUDENT}>
+            <Route index element={<StudentList />} />
+            <Route path=":id" element={<StudentDetail />} />
+          </Route>
+          <Route
+            path={ROUTES.ALL_STUDENTS}
+            element={<Navigate to={ROUTES.STUDENT} replace />}
+          />
         </Route>
-         <Route path={ROUTES.ALL_STUDENTS} element={<Navigate to={ROUTES.STUDENT} replace />} />
-      </Route>
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Suspense>
   );
 };
 
